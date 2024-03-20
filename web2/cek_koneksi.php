47657f207eb6c6a7cb366b4bc241cebdb3deea78
hermanka <hermanka.beta@gmail.com>
2024-03-20 10:18:13 +0700

    test conn

--- web2/cek_koneksi.php
+++ web2/cek_koneksi.php
@@ -2,10 +2,11 @@
 
 function pdo_connect(){
     $DATABASE_HOST = 'mysql-db';
-    $DATABASE_USER = 'root';
+    $DATABASE_USER = 'db_user';
     $DATABASE_PASS = 'password';
     $DATABASE_NAME = 'badcrud';
     try {
+        echo "Connected to MySQL successfully";
     	return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME, $DATABASE_USER, $DATABASE_PASS);
     } catch(Exception $e) {
         return $e;
